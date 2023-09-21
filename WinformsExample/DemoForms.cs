1a6b46279ee0721009af38ac48c62a806dc14eb5
nareshsunkari99 <123562261+nareshsunkari99@users.noreply.github.com>
2023-09-21 09:56:22 +0530

    form from console

---
+++ WinformsExample/DemoForms.cs
@@ -0,0 +1,17 @@
+﻿//using System;
+//using System.Windows.Forms;
+
+//namespace TestForms
+//{
+//    public class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Form form = new Form();
+//            form.Text = "Hello";
+//            form.MaximizeBox = false;
+//            form.MinimizeBox = false;
+//            form.ShowDialog();
+//        }
+//    }
+//}
